1938e658bdb364fe3b08b66df3d21e50073045c5
9546 <qiubin.shen@hand-china.com>
2019-09-20 00:41:08 +0800

    [ADD]新模块

---
+++ src/test/java/com/example/springBootadminclient/SpringBootAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootadminclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
