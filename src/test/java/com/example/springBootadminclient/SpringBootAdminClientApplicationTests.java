12c201bfe4adb05c3e27f9508ed6ee4633859d9b
9546 <qiubin.shen@hand-china.com>
2019-09-20 19:19:15 +0800

    [ADD]添加spring-boot-admin-client模块

--- src/test/java/com/example/springBootadminclient/SpringBootAdminClientApplicationTests.java
+++ src/test/java/com/example/springBootadminclient/SpringBootAdminClientApplicationTests.java
@@ -1,12 +1,11 @@
 package com.example.springBootadminclient;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootAdminClientApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
